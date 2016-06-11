f9d0e9ba461f1c8c2426a17cbb0d7320b948ea6c
Jamie Bradley <jbwebesign@gmail.com>
2016-06-11 20:59:11 +0100

    removed the content from view, its not needed, added article property in config.json so that articles are only compiled if this flag is true

---
+++ app/articles/example.md
@@ -0,0 +1,8 @@
+---
+title: Article Example
+layout: post.jade
+---
+
+# Example Article
+
+This is an example article which has been written using `markdown`. If you haven't already enabled the article feature then please refer to the `sparky.json` file to build articles.
--- tasks/views.js
+++ tasks/views.js
@@ -1,21 +1,35 @@
 module.exports = (gulp, plugins, config) => {
 
-    // code...
-    gulp.task('views', function () {
+    // Compile views and articles
+    gulp.task('views', () => {
         const stream = gulp.src('./app/views/pages/*.pug')
             .pipe(plugins.pug())
             .pipe(gulp.dest('./dist/'))
 
+            if (config.articles === true) {
+              gulp.start('articles');
+            }
+
         return stream;
     });
 
-    // compile .md to .html
-    // @NOTE => Until gulp-layout supports .pug file types then we will continue to build article templates in .jade format.
-    gulp.task('articles', function() {
+    // Clean the articles directory before compiling articles to .html
+    gulp.task('clean-articles', () => {
+      const stream = gulp.src('./dist/articles/')
+        .pipe(plugins.clean());
+
+      return stream;
+    });
+
+    // compile .md to .html -> articles options must be true in sparky.json
+    // @NOTE => Until gulp-layout supports .pug file types then we will
+    //          continue to build article templates in .jade format.
+    gulp.task('articles', ['clean-articles'], () => {
         const stream = gulp.src('./app/articles/**/*.md')
             .pipe(plugins.frontMatter())
             .pipe(plugins.markdown())
-            .pipe(plugins.layout({ "layout" : "./app/views/layouts/articles/post.jade" }, (file) => { return file.frontMatter; }))
+            .pipe(plugins.layout({ "layout" : "./app/views/layouts/articles/post.jade" },
+                                (file) => { return file.frontMatter; }))
             .pipe(gulp.dest('./dist/articles/'));
 
         return stream;

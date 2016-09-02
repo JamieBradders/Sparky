ec5ce7b7a33d5dbe1d03aece9e6f10d7f73e33a5
Jamie Bradley <jbwebesign@gmail.com>
2016-09-02 13:41:47 +0100

    added babel to the gulp tasks excluded sourcemaps for now

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -4,6 +4,9 @@ module.exports = (gulp, plugins, config) => {
 
   gulp.task('bundle-scripts', () => {
     return gulp.src(jsFiles)
+      .pipe(plugins.babel({
+        presets: ['es2015']
+      }))
       .pipe(plugins.concat('app.js'))
       .pipe(plugins.rename('app.min.js'))
       .pipe(plugins.uglify())
